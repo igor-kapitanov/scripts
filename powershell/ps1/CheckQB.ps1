17bda2344ea2c70b91618354ac80d15b8a1177d5
Igor Kapitanov <kapitanov26@gmail.com>
2023-04-28 16:24:14 -0400

    change checkQB

--- powershell/ps1/CheckQB.ps1
+++ powershell/ps1/CheckQB.ps1
@@ -1,5 +1,13 @@
 while($true) {
     Write-Host "***** Checking QB processes *****" -ForegroundColor Green
+    # Check if QB process is running
+    $qb = Get-Process "QBW32" -ErrorAction SilentlyContinue
+    if (!$qb) {
+        Write-Host "***** start QuickBooks process *****" -ForegroundColor Green
+        Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+        # Start QB if it is not running
+        Start-Process "C:\Program Files\Intuit\QuickBooks Enterprise Solutions 23.0\QBWEnterpriseWholesale.exe"
+    }
     # Check if QBWebConnector process is running
     $qbwebconnector = Get-Process "QBWebConnector" -ErrorAction SilentlyContinue
     if (!$qbwebconnector) {
@@ -8,14 +16,7 @@ while($true) {
         # Start QBWebConnector if it is not running
         Start-Process "C:\Program Files (x86)\Common Files\Intuit\QuickBooks\QBWebConnector\QBWebConnector.exe"
     }
-    # Check if QB process is running
-    $qb = Get-Process "QBW32" -ErrorAction SilentlyContinue
-    if (!$qb) {
-        Write-Host "***** start QuickBooks process *****" -ForegroundColor Green
-        Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-        # Start QB if it is not running
-        Start-Process "C:\Program Files (x86)\Intuit\QuickBooks Enterprise Solutions 21.0\QBW32EnterpriseWholesale.exe"
-    }
+    
     # Wait for 30 minutes
     Start-Sleep -Seconds (30 * 60)
 }
