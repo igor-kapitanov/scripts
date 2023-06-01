bb777beb127b7298bdd4b7336648b1d989f8caa7
Igor Kapitanov <kapitanov26@gmail.com>
2023-06-01 16:49:15 -0400

    ConvertPs1ToBat was changed

--- powershell/ps1/ConvertPs1ToBat.ps1
+++ powershell/ps1/ConvertPs1ToBat.ps1
@@ -1,3 +1,20 @@
+<#
+.SYNOPSIS
+	Converts PowerShell scripts to batch files
+.DESCRIPTION
+	This PowerShell script converts one or more PowerShell scripts to .bat batch files.
+.PARAMETER Filepattern
+	Specifies the file pattern
+.EXAMPLE
+	PS> ./convert-ps2bat *.ps1
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz | License: CC0
+#>
+
+param([string]$Filepattern = "")
+
 function Convert-PowerShellToBatch
 {
     param
@@ -15,5 +32,17 @@ function Convert-PowerShellToBatch
         "@echo off`npowershell.exe -NoExit -encodedCommand $encoded" | Set-Content -Path $newPath -Encoding Ascii
     }
 }
+ 
+try {
+	if ($Filepattern -eq "") { $Filepattern = Read-Host "Enter path to the PowerShell script(s)" }
 
-Get-ChildItem -Path "path to the file" | Convert-PowerShellToBatch
+	$Files = Get-ChildItem -path "$Filepattern"
+	foreach ($File in $Files) {
+		Convert-PowerShellToBatch "$File"
+	}
+	exit 0 # success
+} catch {
+	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	exit 1
+}
+
